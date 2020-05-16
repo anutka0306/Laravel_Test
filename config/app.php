eb2f18f5799f47ad2e440e77e78733d1892041a3
anutka0306 <anutka0306@mail.ru>
2020-05-16 11:32:55 +0300

    php debug bar

--- config/app.php
+++ config/app.php
@@ -139,6 +139,7 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
+        Barryvdh\Debugbar\ServiceProvider::class,
         \App\Providers\MenuProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
